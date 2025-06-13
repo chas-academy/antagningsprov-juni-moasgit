db540e93dad5b08c14df440f212e7a4561784a4d
moasgit <moasjberg@gmail.com>
2025-06-13 14:57:18 +0200

    Update uppg6.js

--- uppg6.js
+++ uppg6.js
@@ -1,11 +1,15 @@
 
-
 function uppg6(){
+    let names = ["namn1", "namn2", "namn3"];
+    for(let i = 0; i < names.length; i++) {
+        console.log(names[i]);
+    }
 
     // Skapa en array med namn som du döper till names
     // Loopa igenom arrayen och skriv ut alla namn i konsolen
 
  
 }
+uppg6();
 
 module.exports = { uppg6 };
