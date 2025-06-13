cb5ed8df6706d7fb3fe204fbf7c8e0554b5e06ad
moasgit <moasjberg@gmail.com>
2025-06-13 14:46:45 +0200

    Update uppg5.js

--- uppg5.js
+++ uppg5.js
@@ -16,8 +16,4 @@ uppg5();
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
     // annars skriv ut "Det andra talet är större än det första"
 
-
-
-}
-
 module.exports = { uppg5 };
