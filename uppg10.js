6b55a1f0cd74713d159daf7cddc51498f1696f4b
moasjberg@gmail.com <moasjberg@gmail.com>
2025-06-17 12:22:40 +0200

    uppgift 9 & 10

--- uppg10.js
+++ uppg10.js
@@ -1,5 +1,30 @@
 function uppg10(){
     const numbers = [5, 12, 7, 10, 20, 9, 11];
+
+    function sortNumbers(numbers) {
+      let bigEven = [];
+      let smallOdd = [];
+      let other = [];
+
+      numbers.forEach(function(number) {
+        if (number > 10 && number % 2 === 0) {
+          bigEven.push(number);
+        } else if (number < 10 && number % 2 !== 0) {
+          smallOdd.push(number);
+        } else {
+          other.push(number);
+        }
+      });
+
+      return {
+        bigEven: bigEven,
+        smallOdd: smallOdd,
+        other: other
+      };
+    }
+
+    const result = sortNumbers(numbers);
+    console.log(result);
   
     // skapa tre arrayer - bigEven, smallOdd, other
     // loopa igenom numbers och placera talen i respektive array
@@ -7,11 +32,9 @@ function uppg10(){
     // smallOdd ska innehålla tal mindre än 10 och udda
     // other ska innehålla alla andra tal
     // returnera ett objekt med de tre arrayerna
-
-  
-    
   
   }
+  uppg10();
   
   module.exports = { uppg10 };
   
--- uppg9.js
+++ uppg9.js
@@ -1,6 +1,19 @@
 
 
 function uppg9 (){
+    let numbers = [1, 2, 3, 4, 5];
+
+ function sort(numbers) {
+            numbers.forEach(function(number) {
+                if (number % 2===0) {
+                    console.log(number + " jämt");
+                } else {
+                   console.log(number + " udda");
+                }
+                });
+            }
+        sort(numbers);
+
     // skapa en funktion som heter sort och tar in en array av siffror som argument
     // i funktionen du loopa igenom arrayen och kolla med en if-sats om  talet är jämt eller udda
     // i if-sattsen skriv ut i konsolen resultatet
@@ -10,5 +23,6 @@ function uppg9 (){
 
   
 }
+uppg9();
 
 module.exports = { uppg9 };
